955194b0188e2327296ae064913572b925fc7c41
Jonathan Mares <jm2242@cornell.edu>
2017-03-02 22:45:09 -0500

    axe insta feed, comment out instagram thing, and script source

--- javascript/instagram.js
+++ javascript/instagram.js
@@ -1,9 +1,9 @@
- var userFeed = new Instafeed({
-         get: 'user',
-         userId: '3107725209',
-         clientId: '1e35a2d87bdb4edd87788bdbbdfc278b',
- 		resolution: 'thumbnail',
- 		limit: '16',
- 		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
-     });
-     userFeed.run();
+ // var userFeed = new Instafeed({
+ //         get: 'user',
+ //         userId: '3107725209',
+ //         clientId: '1e35a2d87bdb4edd87788bdbbdfc278b',
+ // 		resolution: 'thumbnail',
+ // 		limit: '16',
+ // 		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
+ //     });
+ //     userFeed.run();
